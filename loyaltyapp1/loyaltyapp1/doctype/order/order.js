e85478c9d47f7df75a2de574a38ca7d2c996dadc
Ravindra Lakal <ravindra.lakal@gmail.com>
2016-01-23 16:10:11 +0530

    Added the username (cashier) a link field to user then fetched the store name using add_fetch which is used to fetch any coloum from linked table to display on the derived table

--- loyaltyapp1/loyaltyapp1/doctype/order/order.js
+++ loyaltyapp1/loyaltyapp1/doctype/order/order.js
@@ -1,9 +1,11 @@
 cur_frm.add_fetch("ean", "mrp", "mrp");
+cur_frm.add_fetch("cashier", "store", "store_id");
 var amount=0
 frappe.ui.form.on("Order", "onload", function(frm) {
   console.log("Page loaded")
 
 frm.set_value("cashier",frappe.user.name)
+
 });
 
 frappe.ui.form.on("Product Details","quantity",function(frm,cdt,cdn){
@@ -13,7 +15,7 @@ frappe.ui.form.on("Product Details","quantity",function(frm,cdt,cdn){
   d.total=d.mrp*d.quantity
   refresh_field("product_details")
   amount=amount+d.total
-    console.log(amount)
+
     frm.set_value("amount",amount)
 
 });
