d808f0b674c857e2f68aa67df6ed9caad8241879
Ravindra Lakal <ravindra.lakal@gmail.com>
2016-01-25 16:46:02 +0530

    Now purchase date and points also reflect in points details child table of customer done by using customer.append() function

--- loyaltyapp1/loyaltyapp1/doctype/order/order.js
+++ loyaltyapp1/loyaltyapp1/doctype/order/order.js
@@ -1,20 +1,22 @@
 cur_frm.add_fetch("ean", "mrp", "mrp");
 cur_frm.add_fetch("cashier", "store", "store_id");
-var amount=0
+  var amount=0
 frappe.ui.form.on("Order", "onload", function(frm) {
   console.log("Page loaded")
+
+  console.log(amount)
 
 frm.set_value("cashier",frappe.user.name)
 
 });
 
 frappe.ui.form.on("Product Details","quantity",function(frm,cdt,cdn){
-  console.log("Hello")
   var d=locals[cdt][cdn];
 
   d.total=d.mrp*d.quantity
   refresh_field("product_details")
   amount=amount+d.total
+  console.log(amount)
 
     frm.set_value("amount",amount)
 
