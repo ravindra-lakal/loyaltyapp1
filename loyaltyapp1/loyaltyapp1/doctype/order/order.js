1b2bc652229b00eb400b0fa1c2874a9634c283a1
Ravindra Lakal <ravindra.lakal@gmail.com>
2016-01-29 17:56:18 +0530

    Added current datetime on the trigger of purchase date

--- loyaltyapp1/loyaltyapp1/doctype/order/order.js
+++ loyaltyapp1/loyaltyapp1/doctype/order/order.js
@@ -2,9 +2,9 @@ cur_frm.add_fetch("ean", "mrp", "mrp");
 cur_frm.add_fetch("cashier", "store", "store_id");
   var amount=0
 frappe.ui.form.on("Order", "onload", function(frm) {
-  console.log("Page loaded")
+//  console.log("Page loaded")
 
-  console.log(amount)
+//  console.log(amount)
 
 frm.set_value("cashier",frappe.user.name)
 
@@ -16,17 +16,25 @@ frappe.ui.form.on("Product Details","quantity",function(frm,cdt,cdn){
   d.total=d.mrp*d.quantity
   refresh_field("product_details")
   amount=amount+d.total
-  console.log(amount)
+  //console.log(d.total)
 
     frm.set_value("amount",amount)
 
 });
-frappe.ui.form.on("Product Details", "product_details_remove", function(frm,cdt,cdn) {
-  var d=locals[cdt][cdn];
-  console.log(d)
+frappe.ui.form.on("Product Details", "product_details_remove", function(frm) {
+
+  var d=locals[cdt][cdn]
+  //console.log(d)
 
   // console.log(amount)
 
 //frm.set_value("cashier",frappe.user.name)
+
+});
+frappe.ui.form.on("Order", "purchase_date", function(frm) {
+frm.set_value("purchase_date",frappe.datetime.now_datetime())
+console.log("Hi")
+
+
 
 });
