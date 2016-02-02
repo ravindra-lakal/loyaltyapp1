16fd485e23f71c9cfa053500cb1322fe70e564ca
Ravindra Lakal <ravindra.lakal@gmail.com>
2016-02-02 15:06:20 +0530

    made the doc no same as name field in order it is now a read only field and doc no. will automatically apear after order is submitted added a field to search Doc No in case of return type orders

--- loyaltyapp1/loyaltyapp1/doctype/order/order.py
+++ loyaltyapp1/loyaltyapp1/doctype/order/order.py
@@ -11,8 +11,8 @@ class Order(Document):
 	def on_update(self):
 		amount=0
 		# self.get is used to access child table values in python script
-		for col in self.get("product_details"):
-			amount+=col.total
+		for raw in self.get("product_details"):
+			amount+=int(raw.total)
 		self.amount=amount
 
 
@@ -20,7 +20,7 @@ class Order(Document):
 	def before_submit(self):
 
 		a=frappe.get_all("Rule Engine", fields=["rule_type","amount","points ","points_multiplication_factor"], filters={"status":"Active","docstatus":1})
-		# docstatus is 1 when document is submitted 
+		# docstatus is 1 when document is submitted
 		for i in a:
 			if i.get('rule_type')=="Loyalty Points":
 					minamount=int(i.get('amount'))
@@ -31,7 +31,9 @@ class Order(Document):
 					a=factor*points
 					self.amount=amount
 					self.points_earned=a
+					self.doc_no=self.name
 	def on_submit(self):
+		now=0
 		customer=frappe.get_doc("Customer",self.username)
 
 
@@ -39,5 +41,10 @@ class Order(Document):
 		n1 = customer.append('points_details', {})
 		n1.purchase_date=self.purchase_date
 		n1.poins_gained=self.points_earned
-		customer.total_points=self.points_earned
+		#customer.total_points=self.points_earned
 		customer.save()
+		for raw in customer.get("points_details"):
+			frappe.errprint(raw.purchase_date)
+			# now+=int(raw.points_gained)
+		customer.total_points=now
+#points_gained is used just for experimental purpose actually a coloum called total points will be added in place of points_gained
