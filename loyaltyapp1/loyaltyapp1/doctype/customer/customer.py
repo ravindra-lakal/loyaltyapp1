de224fb563bdbdd8cee995d6e2d6df9658c7e176
Ravindra Lakal <ravindra.lakal@gmail.com>
2016-02-08 12:25:14 +0530

    Total avilable points can be displayed now i.e sum of points consumed -sum of points gained

--- loyaltyapp1/loyaltyapp1/doctype/customer/customer.py
+++ loyaltyapp1/loyaltyapp1/doctype/customer/customer.py
@@ -5,6 +5,14 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe import _
 
 class Customer(Document):
-	pass
+	def validate(self):
+		points_gained=0
+		points_consumed=0
+		total_points=0
+		for raw in self.get("points_details"):
+			points_gained+=int(raw.points_gained)
+			points_consumed+=int(raw.points_consumed)
+		self.total_points=points_gained - points_consumed
